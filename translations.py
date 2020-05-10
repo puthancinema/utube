88f46f001fb06351c33c230d5013e2cb5d6856be
puthancinema <62384649+puthancinema@users.noreply.github.com>
2020-05-10 15:01:49 +0530

    Update translations.py

--- translations.py
+++ translations.py
@@ -1,7 +1,7 @@
 
 class Messages:
 
-    START_MSG = "Hi there {}.\n\nI'm Youtube Uploader Bot.You can use me to upload any telegram video to youtube once you authorise me.You can know more from /help.\n\nThank you."
+    START_MSG = "Hi there {}.\n\nI'm Youtube Uploader Bot.You can use me to upload any telegram video to youtube once you authorise me.You can know more from /help.\n\nThank you💠."
 
     HELP_MSG = [
         ".",
